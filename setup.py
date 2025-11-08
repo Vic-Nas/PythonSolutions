dd9e493de5725d18608029b54500ee1510db1d4e
Vic-Nas <github-actions@github.com>
2025-11-08 23:15:09 +0000

    Auto-generate docs and bump version to 0.17 [skip ci]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vicutils",
-    version="0.16",
+    version="0.17",
     packages=find_packages(),
     install_requires=[],
     
