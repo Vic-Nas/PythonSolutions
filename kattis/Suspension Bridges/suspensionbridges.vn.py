35e585ac17960a2514d7e2464f1036ab944a4027
Vic-Nas <nasci.victorio@gmail.com>
2025-11-30 00:21:22 -0500

    Translated solution

--- kattis/Suspension Bridges/suspensionbridges.vn.py
+++ kattis/Suspension Bridges/suspensionbridges.vn.py
@@ -7,23 +7,23 @@ from math import cosh, ceil, log2
 d, s = map(int, input().split())
 
 # > 0.1 will fail :( 
-# (Math says it)
+# (Math says so)
 tol = 10 ** -4
 
 def f(epsilon):
-    # epsilon = a / (a + s) | in 0, 1
+    # epsilon = a / (a + s) | in (0, 1)
     return cosh(d*(1-epsilon)/(2*s*epsilon))-1/epsilon # Equation 1
     
 def l(epsilon):
     return 2*s*((1+epsilon)/(1-epsilon))**0.5 # Equation 2
 
 left, right = 0, 1
-# Fancy math. On peut juste iterer 50 fois
-# d_l/d_epsilon pour savoir tol_epsilon(tol_l)
+# Fancy math. We could just iterate 50 times
+# d_l/d_epsilon to figure out tol_epsilon(tol_l)
 tol_epsilon = (tol ** 3) / (2 * s)
 iterations = ceil(log2(1/tol_epsilon - 2))
-# Btw, for bs la taille de l'intervalle est (born_sup - born_inf) / step
-# step = 1 pour les entiers.
+# Btw, for binary search the interval size is (upper_bound - lower_bound) / step
+# step = 1 for integers.
 
 for _ in range(iterations):
     epsilon = (left + right) / 2
