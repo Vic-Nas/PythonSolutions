d6c21ae00063cb08cd3e25adca761fd8047169d2
Vic-Nas <nasci.victorio@gmail.com>
2025-12-07 19:55:44 -0500

    Cleaned

--- aoc/2025/07/07.vn.py
+++ aoc/2025/07/07.vn.py
@@ -9,30 +9,27 @@ with open("input.txt") as file:
         file.readlines()
     ))
     
+nRows = len(inp)
+nCols = len(inp[0])
 
-res1, res2 = 0, 1
-
-from copy import deepcopy
-rows = deepcopy(inp)
-
-for i, row in enumerate(rows[:-2]):
+res1 = 0
+for i, row in enumerate(inp[:-2]):
     if i % 2 == 1: continue
     for j, el in enumerate(row):
         if el in ["^", "."]: continue
-        down = rows[i + 2][j]
+        down = inp[i+2][j]
         if down != "^":
-            rows[i + 2][j] = "|"
+            inp[i+2][j] = "|"
         else:
             res1 += 1
-            if j - 1 >= 0:
-                rows[i + 2][j - 1] = "|"
+            if j-1 >= 0:
+                inp[i+2][j-1] = "|"
 
-            if j + 1 < len(row):
-                rows[i + 2][j + 1] = "|"
+            if j + 1 < nCols:
+                inp[i+2][j+1] = "|"
 
 
-nRows = len(inp)
-nCols = len(inp[0])
+
 
 # dp[i][j] = number of ways to reach position (i, j)
 dp = [[0 for _ in range(nCols)] for _ in range(nRows)]
@@ -41,13 +38,13 @@ dp = [[0 for _ in range(nCols)] for _ in range(nRows)]
 startCol = inp[0].index("S")
 dp[0][startCol] = 1  # 1 way to start
 
-for i in range(0, nRows - 2, 2):
+for i in range(0, nRows-2, 2):
     for j in range(nCols):
         if dp[i][j] == 0:  # No way to get here
             continue
         
         cpt = dp[i][j]  # Number of ways to get here
-        downRow = i + 2
+        downRow = i+2
         down = inp[downRow][j]
         
         if down != "^":
@@ -55,14 +52,14 @@ for i in range(0, nRows - 2, 2):
             dp[downRow][j] += cpt
         else:
             # Can go left or right
-            if j > 0 and inp[downRow][j - 1] != "|":
-                dp[downRow][j - 1] += cpt
+            if j > 0 and inp[downRow][j-1] != "|":
+                dp[downRow][j-1] += cpt
             
-            if j + 1 < nCols and inp[downRow][j + 1] != "|":
-                dp[downRow][j + 1] += cpt
+            if j + 1 < nCols and inp[downRow][j+1] != "|":
+                dp[downRow][j+1] += cpt
 
 # Sum all positions in the last row
-lastRow = nRows - 2
+lastRow = nRows-2
 res2 = sum(dp[lastRow])
 
 print(res1, res2)
