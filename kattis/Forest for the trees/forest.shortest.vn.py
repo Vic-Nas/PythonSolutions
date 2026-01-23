29a12568da57343d6447520c8497bb39a877f56b
Vic-Nas <nasci.victorio@gmail.com>
2026-01-22 21:14:47 -0500

    Updated

--- kattis/Forest for the trees/forest.shortest.vn.py
+++ kattis/Forest for the trees/forest.shortest.vn.py
@@ -1,6 +1,6 @@
 from math import gcd;a,b=map(int,input().split());c=gcd(a,b);a//=c;b//=c;d,e,f,j=map(int,input().split())
-d,f=sorted([d,f])
-e,j=sorted([e,j])
+d,f=[d,f].sort()
+e,j=[e,j].sort()
 k=1
 while k<c:
  x=k*a;y=k*b;k+=1
